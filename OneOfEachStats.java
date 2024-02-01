e4bf6b3cfcee79332a6d06b9ee8b8ee50cd7e716
omrikantor <157234621+omrikantor@users.noreply.github.com>
2024-02-01 18:10:30 +0200

    hi5

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,7 +14,17 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		double a = generator.nextDouble();
+		
+		//// In the previous version of this program, you used a statement like:
+		//// double rnd = Math.random();
+		//// Where "rnd" is the variable that stores the generated random value.
+		//// In this version of the program, replace this statement with:
+		//// double rnd = generator.nextDouble();
+		//// This statement will generate a random value in the range [0,1),
+		//// just like you had in the previous version, except that the 
+		//// randomization will be based on the given seed.
+		//// This is the only change that you have to do in the program.
+		int a = Integer.parseInt(args[0]);
 		int n1 = 0;
 		int n2 = 0;
 		int n3 = 0;
@@ -85,5 +95,5 @@ public class OneOfEachStats {
 				System.out.println("The most common number of children is 4 or more.");
 			}
 		}
-		}
 	}
+}
